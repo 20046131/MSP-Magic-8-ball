acfbc948ff9760085eeaddb10d9c1cbb6332044e
20046131 <69453644+20046131@users.noreply.github.com>
2021-11-13 00:36:33 +0800

    User input with the server responding with a selected responses

--- client.py
+++ client.py
@@ -1,15 +1,17 @@
 import socket
 from server import server
-
+userinput= True
 def client():
-    # Create client socket (AF_INET => IP, SOCK_STREAM => TCP)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('127.0.0.1', 5000))
-    sock.send("I am LEGEND<br>".encode())
-    from_server = sock.recv(4096)
-    sock.close()
-    print(from_server.decode())
+    while userinput:
+        usersinput = input("Enter your question: ")
+        # Create client socket (AF_INET => IP, SOCK_STREAM => TCP)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(('127.0.0.1', 5000))
+        sock.send(usersinput.encode()) # encodes the userinput and will sent it to the server
+        from_server = sock.recv(4096)
+        sock.close() # closes the socket to the server
+        print(from_server.decode())  # prints the server response to the usersinput
 
-
+# Run the sever before running the main
 if __name__ == '__main__':
     client()
